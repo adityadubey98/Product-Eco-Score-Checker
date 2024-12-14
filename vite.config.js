d4f5d7b19c1d3eb467c108f5e770038287e35f1f
Aditya Dubey <dubeyadi9661@gmail.com>
2024-12-14 22:56:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'Product-Eco-Score-Checker',
   plugins: [react()],
   optimizeDeps: {
     include: ['chart.js'],
